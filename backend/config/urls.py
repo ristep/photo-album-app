eb1a7660c6e073b5a4414ff8164d0d10092e3b28
ristep <ristepan@gmail.com>
2025-12-07 19:08:59 +0100

    Home Page added

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,10 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.albums.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
 ]
 
 if settings.DEBUG:
